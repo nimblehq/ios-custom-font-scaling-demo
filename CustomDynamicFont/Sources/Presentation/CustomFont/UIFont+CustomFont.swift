5a2dacf3ce93d8a3ebe7c276975f4e6876bda795
Bliss Pisit Wetcha <pisit@nimblehq.co>
2021-11-05 16:26:42 +0700

    [#2] Add custom font

---
+++ CustomDynamicFont/Sources/Presentation/CustomFont/UIFont+CustomFont.swift
@@ -0,0 +1,23 @@
+//
+//  UIFont+CustomFont.swift
+//  CustomDynamicFont
+//
+//  Created by Bliss on 5/11/21.
+//  Copyright © 2021 Nimble. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    enum ZenOldMincho: String {
+        
+        case regular = "ZenOldMincho-Regular"
+        case bold = "ZenOldMincho-Bold"
+        
+        func size(_ size: CGFloat) -> UIFont? {
+            UIFont(name: self.rawValue, size: size)
+        }
+    }
+}
+
