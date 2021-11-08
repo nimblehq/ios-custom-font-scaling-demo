2a3484ca703f17f8b32366b1f4502dea9ef77f3f
Bliss Pisit Wetcha <pisit@nimblehq.co>
2021-11-08 13:06:05 +0700

    [#4] Rename title

---
+++ CustomDynamicFont/Sources/Presentation/Modules/SelectOSViewController/SelectOSViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SelectOSViewController.swift
+//  CustomDynamicFont
+//
+//  Created by Bliss on 8/11/21.
+//  Copyright © 2021 Nimble. All rights reserved.
+//
+
+import UIKit
+
+final class SelectOSViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Dynamic Font"
+    }
+}
