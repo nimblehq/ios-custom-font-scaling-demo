d95cbba73bced271bff36ed8f2e214d61a71fc13
Bliss Pisit Wetcha <pisit@nimblehq.co>
2021-11-17 18:09:06 +0700

    [#20] add more test to viewmodel

--- CustomDynamicFontTests/Sources/Specs/IOS11UIKit/UIKitViewModelSpec.swift
+++ CustomDynamicFontTests/Sources/Specs/IOS11UIKit/UIKitViewModelSpec.swift
@@ -5,7 +5,7 @@
 //  Created by Bliss on 17/11/21.
 //  Copyright © 2021 Nimble. All rights reserved.
 //
-// swiftlint:disable closure_body_length
+// swiftlint:disable closure_body_length function_body_length
 
 @testable import CustomDynamicFont
 import Nimble
@@ -65,6 +65,94 @@ final class UIKitViewModelSpec: QuickSpec {
                     expect(viewModel.output.caption)
                         .events(scheduler: scheduler, disposeBag: disposeBag)
                         .to(equal([.next(0, R.string.localizable.ios11uikitCommentlabelTitle()), .completed(0)]))
+                }
+
+                it("returns output with correct overrideFontTitle") {
+                    expect(viewModel.output.overrideFontTitle)
+                        .events(scheduler: scheduler, disposeBag: disposeBag)
+                        .to(equal([.next(0, R.string.localizable.ios11uikitOverrideFontTitleTitle()), .completed(0)]))
+                }
+
+                it("returns output with correct overrideFontScaleTitle") {
+                    expect(viewModel.output.overrideFontScaleTitle)
+                        .events(scheduler: scheduler, disposeBag: disposeBag)
+                        .to(equal([
+                            .next(0, R.string.localizable.ios11uikitOverrideFontScaleTitleTitle()),
+                            .completed(0)
+                        ]))
+                }
+
+                it("returns output with correct overrideFontSegmentOptions") {
+                    expect(viewModel.output.overrideFontSegmentOptions)
+                        .events(scheduler: scheduler, disposeBag: disposeBag)
+                        .to(equal([
+                            .next(
+                                0,
+                                [
+                                    R.string.localizable.ios11uikitOverrideFontSegmentOptionsOn(),
+                                    R.string.localizable.ios11uikitOverrideFontSegmentOptionsOff()
+                                ]
+                            ),
+                            .completed(0)
+                        ]))
+                }
+
+                it("returns output with correct overrideFontSliderOptions") {
+                    expect(viewModel.output.overrideFontSliderOptions)
+                        .events(scheduler: scheduler, disposeBag: disposeBag)
+                        .to(equal([.next(0, [.small, .medium, .large, .extraLarge]), .completed(0)]))
+                }
+            }
+
+            context("when output overrideFontOnValue is changed") {
+
+                it("set correct value to output overrideFontIsHidding") {
+                    scheduler.scheduleAt(50) {
+                        viewModel.output.overrideFontOnValue.accept(1)
+                    }
+                    scheduler.scheduleAt(100) {
+                        viewModel.output.overrideFontOnValue.accept(0)
+                    }
+
+                    expect(viewModel.output.overrideFontIsHidding)
+                        .events(scheduler: scheduler, disposeBag: disposeBag)
+                        .to(equal([.next(0, true), .next(50, false), .next(100, true)]))
+                }
+
+                it("set correct value to output overrideFontSize") {
+                    scheduler.scheduleAt(50) {
+                        viewModel.output.overrideFontOnValue.accept(1)
+                    }
+                    scheduler.scheduleAt(100) {
+                        viewModel.output.overrideFontOnValue.accept(0)
+                    }
+
+                    expect(viewModel.output.overrideFontSize)
+                        .events(scheduler: scheduler, disposeBag: disposeBag)
+                        .to(equal([.next(50, .medium), .next(100, nil)]))
+                }
+            }
+
+            context("when input overrideFontSize is called") {
+
+                it("returns output overrideFontSize with correct value") {
+                    scheduler.scheduleAt(50) {
+                        viewModel.input.overrideFontSize(0)
+                    }
+
+                    expect(viewModel.output.overrideFontSize)
+                        .events(scheduler: scheduler, disposeBag: disposeBag)
+                        .to(equal([.next(50, .small)]))
+                }
+
+                it("returns output overrideFontSizeText with correct value") {
+                    scheduler.scheduleAt(50) {
+                        viewModel.input.overrideFontSize(0)
+                    }
+
+                    expect(viewModel.output.overrideFontSizeText)
+                        .events(scheduler: scheduler, disposeBag: disposeBag)
+                        .to(equal([.next(50, UIContentSizeCategory.small.label())]))
                 }
             }
         }
