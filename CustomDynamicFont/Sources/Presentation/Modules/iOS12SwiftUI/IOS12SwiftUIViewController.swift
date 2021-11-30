76e33652ec76399da49ef1b78e0df9bbbeba1c4b
Bliss Pisit Wetcha <pisit@nimblehq.co>
2021-11-30 09:57:05 +0700

    [#15] remove unused prefix

--- CustomDynamicFont/Sources/Presentation/Modules/iOS12SwiftUI/IOS12SwiftUIViewController.swift
+++ CustomDynamicFont/Sources/Presentation/Modules/iOS12SwiftUI/IOS12SwiftUIViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 @available(iOS 13.0, *)
 final class IOS12SwiftUIViewController: UIHostingController<DynamicFontSwiftUIView> {
 
-    @MainActor @objc dynamic required init?(coder _: NSCoder) {
+    required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
